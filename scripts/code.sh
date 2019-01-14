be2041a4170a9d78b5da6f9a03cbbcf196c4c99b
jeong <tbskthddl@naver.com>
2019-01-14 11:14:56 +0900

    생각을 하면서 코드를 수정함.

---
+++ scripts/code.sh
@@ -0,0 +1,6 @@
+mport https://packages.microsoft.com/keys/microsoft.asc
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+yum check-update
+yum -y  install code#!/bin/sh
+
