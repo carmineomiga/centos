cce5714a0f706e43dc82d07efbe3003b9d268d69
jeong <tbskthddl@naver.com>
2019-01-24 12:00:09 +0900

    choose better way.

--- bin/addproject.py
+++ bin/addproject.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 #coding:utf8
 
-# === my way ===
-
-# import sys
-# import os
-
-# p = "project"
-# name = sys.argv[1]
-
-# os.makedirs("/%s/%s/asset/char" % (p, name))
-# os.makedirs("/%s/%s/asset/shader" % (p, name))
-# os.makedirs("/%s/%s/config/ocio" % (p, name))
-# os.makedirs("/%s/%s/config/thumbnail" % (p, name))
-# os.makedirs("/%s/%s/doc/concept" % (p, name))
-# os.makedirs("/%s/%s/edit" % (p, name))
-# os.makedirs("/%s/%s/in" % (p, name))
-# os.makedirs("/%s/%s/out" % (p, name))
-# os.makedirs("/%s/%s/shot" % (p, name))
-
-# === other way ===
-
 import os
 import sys
 
