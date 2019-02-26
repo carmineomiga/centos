e2e5161ce6446c76a6fc6153b8867e5c9a44cd41
j_eong <tbskthddl@naver.com>
2019-02-26 15:58:24 +0900

    리팩토링

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 echo "source /home/$USER/centos/env/init.env" >> /home/$USER/.bashrc
 echo "source /home/$USER/cenots/env/help.env" >> /home/$USER/.bashrc
+
+if [ ! -d "$HOME/bin" ]; then
+	mkdir $HOME/bin
+fi
+cp ./bin/* $HOME/bin
+LANG=C xdg-user-dirs-gtk-update
+
+git config --global user.name "carmineomiga"
+git config --global user.email tbskthddl@naver.com
+git config --global color.ui true
